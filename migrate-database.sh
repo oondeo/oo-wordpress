f84defc3d73a701b6dd04221a829c4547b1fcbcf
Amon-ra <amon.raj@gmail.com>
2016-02-18 13:41:41 +0100

    fixed permisions

--- migrate-database.sh
+++ migrate-database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wp core config --dbname='getenv("DATABASE_NAME")' --dbuser='getenv("DATABASE_USER")' --dbpass='getenv("DATABASE_PASSWORD")' --dbhost='getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST")' --skip-check
-sed -i "s/.getenv\(.*\)..\;/getenv\1);/g" wp-config.php
+sed -i "s/.getenv\(.*\)..\;/getenv\1);/g" ${HOME}/wp-config.php
 # wp core install --url=${APPLICATION_DOMAIN} --title=${APPLICATION_DOMAIN} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}  --skip-email
 
 # first='0'
