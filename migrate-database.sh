6d7a651b6869d861fcbbe4a9982cfa24b04749ed
Amon-ra <amon.raj@gmail.com>
2016-02-19 13:30:09 +0100

    install wordress in subdir and fix permisions

--- migrate-database.sh
+++ migrate-database.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-wp core config --dbname='getenv("DATABASE_NAME")' --dbuser='getenv("DATABASE_USER")' --dbpass='getenv("DATABASE_PASSWORD")' --dbhost='getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST")' --skip-check
-sed -i "s/.getenv\(.*\)..\;/getenv\1);/g" ${HOME}/wp-config.php
-# wp core install --url=${APPLICATION_DOMAIN} --title=${APPLICATION_DOMAIN} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}  --skip-email
+
+wp core install --url=${APPLICATION_DOMAIN} --title=${APPLICATION_DOMAIN} --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL}  --skip-email
 
 # first='0'
 # for i in $(echo $WORDPRESS_THEMES | tr "," "\n")
