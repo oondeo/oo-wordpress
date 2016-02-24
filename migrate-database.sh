924f9c7604fee7283771cebab96b09db95039bda
Amon-ra <amon.raj@gmail.com>
2016-02-24 19:02:24 +0100

    make scripts executables

--- migrate-database.sh
+++ migrate-database.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-wp core install --path=www --url=${APPLICATION_DOMAIN} --title=${APPLICATION_DOMAIN} --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL}  --skip-email
+# wp core install --path=www --url=${APPLICATION_DOMAIN} --title=${APPLICATION_DOMAIN} --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL}  --skip-email
 
-first='0'
-for i in $(echo $WORDPRESS_THEMES | tr "," "\n")
-do
-    if [ $first == '0' ]; then 
-        wp theme install --path=www $i --activate
-        first='1'
-    else
-        wp theme install --path=www $i
-    fi 
-done
+# first='0'
+# for i in $(echo $WORDPRESS_THEMES | tr "," "\n")
+# do
+#     if [ $first == '0' ]; then 
+#         wp theme install --path=www $i --activate
+#         first='1'
+#     else
+#         wp theme install --path=www $i
+#     fi 
+# done
 
-for i in $(echo $WORDPRESS_PLUGINS | tr "," "\n")
-do
-    wp plugin install --path=www $i
-done 
+# for i in $(echo $WORDPRESS_PLUGINS | tr "," "\n")
+# do
+#     wp plugin install --path=www $i
+# done 
 
